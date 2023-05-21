9e0aabbbc83d952d44308f56aeaa579a2def273e
jaem1n207 <roy.jm.lee@gmail.com>
2023-05-21 20:11:51 +0900

    fix: jest 구성 수정

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@
 export default {
   verbose: true,
   moduleFileExtensions: ['js', 'json'],
+  testPathIgnorePatterns: ['node_modules/', 'src/utils/'],
+  coveragePathIgnorePatterns: ['node_modules/', 'src/utils/'],
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   transform: {
@@ -10,5 +12,6 @@ export default {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
   coverageDirectory: './coverage',
+  coverageReporters: ['json-summary', 'text', 'lcov'],
   collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
 };
