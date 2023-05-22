0d8ab072b3bfecaf5985f032759db12af73af59e
jaem1n207 <roy.jm.lee@gmail.com>
2023-05-23 01:35:55 +0900

    feat: 테스트 케이스 추가

--- src/uncategorized/condition-string/__test__/condition-string.test.js
+++ src/uncategorized/condition-string/__test__/condition-string.test.js
@@ -5,7 +5,15 @@ describe('conditionString', () => {
     expect(conditionString('<', '=', 20, 50)).toEqual(1);
   });
 
-  it('41 >= 78이면 0을 반환한다.', () => {
+  it('41 > 78이면 0을 반환한다.', () => {
     expect(conditionString('>', '!', 41, 78)).toEqual(0);
+  });
+
+  it('50 >= 50이면 1을 반환한다.', () => {
+    expect(conditionString('>', '=', 50, 50)).toEqual(1);
+  });
+
+  it('30 < 30이면 0을 반환한다.', () => {
+    expect(conditionString('<', '!', 30, 30)).toEqual(0);
   });
 });
